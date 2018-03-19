aff6331d84b33f8bb64d375e5efa04a995312526
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-03-18 17:21:32 -0700

    BUILT SPIKE NODE BACKEND * Recieves AJAX request from Main Backend * Emits Socket.IO TCP events to update Frontend

---
+++ spike/routes/gate-route.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const bodyParser = require('body-parser').json();
+
+module.exports = router => {
+  router.route('/calc')
+    .post(bodyParser, (req, res) => {
+      console.log('Second Backend Hit');
+      console.log('Req Body', req.body);
+      router.socketServer.emit('UPDATE_CHAIN', req.body.hello);
+      res.sendStatus(204);
+    });
+};
