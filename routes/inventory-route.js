75b7946baf51610ce50e98ac635b8162e5b7ac59
sundownspecial <je.mendoza@live.com>
2018-03-21 16:57:08 -0700

    changed to stuff

--- routes/inventory-route.js
+++ routes/inventory-route.js
@@ -8,9 +8,12 @@ const tools = require('../lib/tools')
 module.exports = router => {
   router.route('/inventory/:search')
     .get(bodyParser, (req, res) => {
-        console.log(req.params.search,'params search')
-        tools.urlParser(req.params.search)
-        .then(data => Transaction.find(data))
+ 
+       console.log(req.params.search,'params search')
+        console.log(tools.urlParser)
+        let stuff = tools.urlParser(req.params.search)
+        
+        Transaction.find(stuff)
         .then(data => res.send(data))
         .catch(err => console.error(err));
       // router.socketServer.emit('UPDATE_CHAIN', req.body);
